c81fd7d90d2a61425b0d3f67ba46d9964da88aa8
Łukasz <lukasz662@gmail.com>
2019-01-07 16:40:13 +0100

    Dodany niedziałający toolbar

--- app/src/main/java/com/waga/waga01/MainActivity.kt
+++ app/src/main/java/com/waga/waga01/MainActivity.kt
@@ -5,7 +5,14 @@ import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
 import android.util.TypedValue
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.TableLayout
@@ -18,13 +25,69 @@ class MainActivity : AppCompatActivity() {
     val products : ArrayList<Produkt> = ArrayList()
     var login: String = " "
     var pass: String = " "
+    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        /*val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }*/
+
         login = intent.getStringExtra("Login")
         pass = intent.getStringExtra("Password")
+
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+
+        /*mDrawerLayout.addDrawerListener(
+            object : DrawerLayout.DrawerListener {
+                override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                    // Respond when the drawer's position changes
+                }
+
+                override fun onDrawerOpened(drawerView: View) {
+                    // Respond when the drawer is opened
+                }
+
+                override fun onDrawerClosed(drawerView: View) {
+                    // Respond when the drawer is closed
+                }
+
+                override fun onDrawerStateChanged(newState: Int) {
+                    // Respond when the drawer motion state changes
+                }
+            }
+        )*/
+
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped (unless it can be bought in każdy sklep)
+            mDrawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // LOL PRZEKLEJANIE KODU ZE STRONY ANDROIDA TAKIE TRUDNE XD
+            // For example, swap UI fragments here
+
+            true
+        }
+
+        fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                android.R.id.home -> {
+                    mDrawerLayout.openDrawer(GravityCompat.START)
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
 
         connect()
         update()
